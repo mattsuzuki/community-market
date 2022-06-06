f963850c719f1493468645e2e7523cd73061ef78
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-06-06 14:24:40 -0700

    create listing form

--- src/pages/CreateListingPage/CreateListingPage.jsx
+++ src/pages/CreateListingPage/CreateListingPage.jsx
@@ -1,16 +1,45 @@
 import { checkToken } from "../../utilities/users-service";
+import { useState } from 'react';
+
 
 export default function CreateListingPage() { 
-    async function handleCheckToken() {
-        const expDate = await checkToken();
-        console.log(expDate);
-  }
-  
-  
-  return (
-    <>
-      <h1>Sell an Item Or Offer a Sevice</h1>
-      {/* <button onClick={handleCheckToken}>Check when my login expires</button> */}
-    </>
-  );
-}
+    const [fields, setFields] = useState({
+        item_name: '',
+        item_description:'',
+        item_price:'',
+    });
+
+    const handleChange = (e) => {
+        const value= e.target.value;
+        const name= e.target.name;
+
+        setFields({
+            ...fields,
+            [name]: value
+        });
+    };
+
+    return (
+        <form>
+            <input
+            onChange={handleChange}
+            name="item_name"
+            placeholder="Enter Item"
+            value={fields.item_name}
+            />
+            <input
+            onChange={handleChange}
+            name="item_description"
+            placeholder="Description"
+            value={fields.item_description}
+            />
+            <input
+            onChange={handleChange}
+            name="item_price"
+            placeholder="price"
+            value={fields.item_price}
+            />
+            <button type="Submit">Submit Listing</button>
+        </form>
+    )
+} 
