036cc5fc489c77ecf44453f2a162bd0948a8bbd5
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-06-08 16:48:06 -0700

    seller id's functional

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -21,6 +21,9 @@ export default function ProfilePage() {
   } else {
     return (
       <>
+        <h1>{user.name}</h1>
+        <h1>{user.phone}</h1>
+        <h1>{user.location}</h1>
         <h1>{user.email}</h1>
       </>
     );
