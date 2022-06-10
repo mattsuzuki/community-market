d04395374676994ce933423f556b2e9018f6cb5c
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-06-10 13:58:37 -0700

    MVP7

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,5 +1,4 @@
-// API modules are where the code lives to communicate
-// with the server via AJAX
+
 import sendRequest from './send-request';
 const BASE_URL = '/api/users';
 
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,27 +1,21 @@
-// Serice modules hold the code that implements
-// "business"/application logic
-// Service methods often depend upon or use
-// methods in the API modules
 
-// Import all named exports
 import * as usersAPI from './users-api';
 
 export async function signUp(userData) {
-  // Delegate the AJAX request to the users-api.js
-  // module.
+
   const token = await usersAPI.signUp(userData);
   localStorage.setItem('token', token);
   return getUser();
 }
 
 export function getToken() {
-  // getItem will return null if the key does not exist
+
   const token = localStorage.getItem('token');
   if (!token) return null;
-  // Let's check if token has expired...
+
   const payload = JSON.parse(atob(token.split('.')[1]));
   if (payload.exp < Date.now() / 1000) {
-    // Token has expired
+
     localStorage.removeItem('token');
     return null;
   }
@@ -41,8 +35,7 @@ export function logOut() {
 }
 
 export async function login(credentials) {
-  // Delegate the AJAX request to the users-api.js
-  // module.
+
   const token = await usersAPI.login(credentials);
   localStorage.setItem('token', token);
   return getUser();
