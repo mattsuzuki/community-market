8dea373daedad56888c1b49a45667db2e8c330c7
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-06-12 11:20:43 -0700

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,8 @@
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service';
-import './NavBar.css'
+import { Link } from "react-router-dom";
+import * as userService from "../../utilities/users-service";
+import "./NavBar.css";
 
 export default function NavBar({ user, setUser }) {
-
   function handleLogOut() {
     // Remove token using the user service
     userService.logOut();
@@ -13,22 +12,28 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to="/auth/login">Login</Link>
-      &nbsp; | &nbsp;
-      <Link to="/auth/signup">Sign up</Link>
-      &nbsp; | &nbsp;
-      <Link to="/newlisting">Create Listing</Link>
-      &nbsp; | &nbsp;
- 
-      <Link to="/listings">Listings</Link>
-      { user &&
+      {!user && (
         <>
+          <Link to="/auth/login">Login</Link>
           &nbsp; | &nbsp;
+          <Link to="/auth/signup">Sign up</Link>
+        </>
+      )}
+
+      {user && (
+        <>
           <span>Welcome, {user.name}</span>
           &nbsp; | &nbsp;
-          <Link to="" onClick={handleLogOut}>Log Out</Link>
+          <Link to="/newlisting">Create Listing</Link>
+          &nbsp; | &nbsp;
+          <Link to="/listings">Listings</Link>
+          &nbsp; | &nbsp;
+          <Link to="" onClick={handleLogOut}>
+            Log Out
+          </Link>
         </>
-      }
+      )}
+      {!user}
     </nav>
   );
 }
